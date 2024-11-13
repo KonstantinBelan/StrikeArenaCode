81fc5e89e343b3f1eae6b4c20e4c514ff853bd39
KonstantinBelan <95037979+KonstantinBelan@users.noreply.github.com>
2024-11-13 08:12:02 +0700

    Update and rename script3-v1.0.0.js to script3-v1.0.1.js

--- script3-v1.0.1.js
+++ script3-v1.0.1.js
@@ -29,7 +29,7 @@
             evt.initUIEvent('resize', true, false, window, 0);
             window.dispatchEvent(evt);
             window.dispatchEvent(new Event('resize'));
-        }, 1500)
+        }, 2000)
     });
 
     if (window.location.href.indexOf("country=") > -1) {
