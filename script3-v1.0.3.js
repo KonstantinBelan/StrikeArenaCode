eb8f6502e3095d19184e4a59b81597a54f931b21
KonstantinBelan <95037979+KonstantinBelan@users.noreply.github.com>
2024-11-13 08:41:49 +0700

    Update and rename script3-v1.0.2.js to script3-v1.0.3.js

--- script3-v1.0.3.js
+++ script3-v1.0.3.js
@@ -21,15 +21,15 @@
                 }, 500)
             }
         }, 5500)
-        // setTimeout(function() {
-        //     document.querySelectorAll('.t-rec').forEach(trec => {
-        //         trec.classList.add('visibled')
-        //     })
-        //     var evt = document.createEvent('UIEvents');
-        //     evt.initUIEvent('resize', true, false, window, 0);
-        //     window.dispatchEvent(evt);
-        //     window.dispatchEvent(new Event('resize'));
-        // }, 2000)
+        setTimeout(function() {
+            document.querySelectorAll('.t-rec').forEach(trec => {
+                trec.classList.add('visibled')
+            })
+            var evt = document.createEvent('UIEvents');
+            evt.initUIEvent('resize', true, false, window, 0);
+            window.dispatchEvent(evt);
+            window.dispatchEvent(new Event('resize'));
+        }, 1000)
     });
 
     if (window.location.href.indexOf("country=") > -1) {
