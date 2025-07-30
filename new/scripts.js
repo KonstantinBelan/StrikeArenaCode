7d43c31c8944f1a7fb5e4f4000da13238429abc4
KonstantinBelan <95037979+KonstantinBelan@users.noreply.github.com>
2025-07-30 15:25:43 +0700

    Update scripts.js

--- new/scripts.js
+++ new/scripts.js
@@ -25,3 +25,28 @@
             }
         });  
     };
+document.addEventListener('DOMContentLoaded', function() {
+  if ('loading' in HTMLIFrameElement.prototype) {
+    setTimeout(function () {
+      const iframes = document.querySelectorAll('iframe[loading="lazy"]')
+
+      iframes.forEach(iframe => {
+        iframe.src = iframe.dataset.src
+      })
+    }, 700)
+	}
+	
+  /* Header Sticky */
+  let headerContainer = document.querySelector('#rec808658909 .t396__artboard')
+  if (headerContainer) {
+    let sticky = headerContainer.offsetTop + 40
+    document.addEventListener('scroll', scrollMenu)
+    function scrollMenu() {
+      if (window.scrollY > sticky) {
+        headerContainer.classList.add('sticky')
+      } else {
+        headerContainer.classList.remove('sticky')
+      }
+    }
+  }
+});
